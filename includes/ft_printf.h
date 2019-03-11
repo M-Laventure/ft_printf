d034d811a9d220e295ec32f83cd6ff4158b7a913
Myriam BENZARTI <mybenzar@e3r12p8.42.fr>
2019-03-11 13:00:23 +0100

    changements mineurs du header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/07 17:50:28 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/03/08 16:03:35 by mybenzar         ###   ########.fr       */
+/*   Created: 2019/03/11 12:52:43 by mybenzar          #+#    #+#             */
+/*   Updated: 2019/03/11 12:59:59 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,16 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-typedef struct	s_struct
+typedef struct	s_format
+{
+	
+}				t_format;
+
+typedef struct	s_print
 {
 	char	(*f[?]) (const char *str);
 
-}				t_struct;
+}				t_print;
 
 int	ft_printf(const char *format, ...);
 
