d6af8df7a83dff46f47e6bbc3d71f6ff8277fdff
Myriam BENZARTI <mybenzar@e3r10p11.42.fr>
2019-03-20 18:38:21 +0100

    push avant diner

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 12:52:43 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/03/20 13:30:27 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/03/20 18:38:14 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,41 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-/*emum id_conv
-{
-	char = 'c', string = 's', pointer = 'p', double = 'd', dec_int = 'i', octal_num = 'o', float_point = 'f', unsigned_dec = 'u', hex_num_lwrcase = 'x', hex_num_uprcase = 'X'
-}*/
-
 # define ABS(x) (x < 0 ? -x : x)
+
+/*
+ * anciens commentaires :
+** emum id_conv
+	{
+		char = 'c', string = 's', pointer = 'p', double = 'd', dec_int = 'i', octal_num = 'o', float_point = 'f', unsigned_dec = 'u', hex_num_lwrcase = 'x', hex_num_uprcase = 'X'
+	}
+
+**	width - (len(param) + plus + change selon la conversion passe ex :pour "%o ou %x" / SI padding < 0 lors du calcul le param est plus grand que la width donc set a 0
+*/
+
+
+typedef enum	e_modif
+{
+	NONE, h, hh, l, ll, L
+}				t_modif;
 
 typedef const char * restrict str_format;
 
 
 typedef struct	s_flags
 {
-	char	*param;
-	char	*str_conv;
-	char	*str_bflags; // la string entre % et le premier flag
-	int		width; //number
-	int		precision; //number (sans le ".")
-	int		diese; //boolean
-  	//width - (len(param) + plus + change selon la conversion passe 
-	//ex :pour "%o ou %x" / SI padding < 0 lors du calcul le param est plus grand que la width donc set a 0
-	int		modif;
-	int		minus;	//left_justified boolean
-	int		plus; //print '+' before printing the parameter boolean
-	char	id_conv; //type de la conversion
-	int		zero //boolean; // si 0 le premier digit apres % alors le padding se fait avec des 0 -> set a ' ' ou '0', 
+	char		*param;
+	char		*str_conv;
+	char		*str_bflags; // la string entre % et le premier flag
+	int			width; //number
+	int			precision; //number (sans le ".")
+	int			diese; //boolean
+
+	t_modif		modif;
+	int			minus;	//left_justified boolean
+	int			plus; //print '+' before printing the parameter boolean
+	char		conv; //type de la conversion
+	int			zero //boolean; // si 0 le premier digit apres % alors le padding se fait avec des 0 -> set a ' ' ou '0', 
 						//les 0 sont forcement au debut, le flag 0 est ignore si flag - mais pas pour les char 
 }				t_flags;
 
--- srcs/str_converter.c
+++ srcs/str_converter.c
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 13:25:45 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/03/20 14:53:32 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/03/20 18:38:14 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	str_converter(t_flags *flag)
 {
-	
+	char	str[100];
+	int		i;
+	int		base;
+
+	if (flag.conv = 'd' || flag.conv = 'i' || flag.conv = 'u')
+		base = 10;
+	if (flag.conv = 'o')
+		base = 8;
+	if (flag.conv = 'x' || flag.conv = 'X' || flag.conv = 'p')
+		base = 16;
+		
+
+	i = 99;
+
 }
