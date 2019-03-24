bfa32ac152b659266ef3fd1dc0632c673728cb8f
Myriam BENZARTI <mybenzar@e3r3p3.42.fr>
2019-03-24 14:58:25 +0100

    zut

--- srcs/get_flags.c
+++ srcs/get_flags.c
@@ -1,42 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_flags.c                                        :+:      :+:    :+:   */
+/*   get_flagss.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 18:04:36 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/03/19 15:57:58 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/03/20 13:36:06 by malavent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	get_flags(t_flags *flag)
+void	get_flags(t_flags *flag, str_format format)
 {
 	int i;
 	int len;
 
-	len = ft_strlen(t_flag.param);
-	if (ft_strchr(t_flags.str_bflags, '+') != NULL)
-		flag.plus = 1;
-	if (ft_strchr(t_flags.str_bflags, ' ') != NULL)
-		flag.space = 1;
-	if (ft_strchr(t_flags.str_bflags, '-') != NULL)
+	len = ft_strlen(t_flags.param);
+	if (ft_strchr(format, '+') != NULL)
+		flags.plus = 1;
+	if (ft_strchr(format, ' ') != NULL)
+		flags.space = 1;
+	if (ft_strchr(format, '-') != NULL)
 	{
-		i = ft_strlen(t_flags.str_bflags) - ft_strlen(ft_strchr(t_flags.str_bflags, '-')) + 1;
-		flag.padding = ft_atoi(t_flags.str_bflags + i);
+		i = ft_strlen(format) - ft_strlen(ft_strchr(format, '-')) + 1;
+		flags.padding = ft_atoi(format + i);
 	}
-	if (ft_strchr(t_flags.str_bflags, '#') != NULL)
+	if (ft_strchr(format, '#') != NULL)
 	{
-		//ici param est considere comme un hexadecimal meme si le flag n'est pas x (undefined behaviour)
-		flag.id_conv = 'x';
-		flag.diese = 1;
+		//ici param est considere comme un hexadecimal meme si le flags n'est pas x (undefined behaviour)
+		flags.id_conv = 'x';
+		flags.diese = 1;
 	}
-	if (ft_strchr(t_flags.str_bflags, '0') != NULL)
+	if (ft_strchr(format, '0') != NULL)
 	{
 		// afficher n caracteres, completes a gauche par des '0'. du coup il faut reussir a recuperer le n
-		flag.zero_fill = n - len;
+		flags.zero_fill = n - len;
 	}
 }
 
