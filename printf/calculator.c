6d62c0a69743c62af23221944be66f752658a83b
Myriam BENZARTI <mybenzar@student.42.fr>
2019-04-04 21:32:27 +0200

    useless

--- printf/calculator.c
+++ printf/calculator.c
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 16:20:44 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/04/04 17:36:00 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/04/04 21:32:10 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,47 +19,89 @@ int		ft_max(int a, int b)
 	return (b);
 }
 
+char	*str_max(char *s1, char *s2, int len1, int len2)
+{
+	if (ft_max(len1, len2) == len1)
+		return (s1);
+	else
+		return (s2);
+}
+
 char	*str_sum(char *s1, char *s2)
 {
 	char	*res;
 	int		hold;
-	int		len_max;
+	int		len;
 	int		len1;
 	int		len2;
-	int		i;
-	//int		j;
+	int		j;
 
-	//j = 0;
 	hold = 0;
-	i = 0;
-	flag = 0;
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	len_max = ft_max(len1, len2) + 1;
-	printf("len = %d\n", len);
+	len = ft_max(len1, len2) + 1;
+	printf("len_max = %d\n", len);
 	if (!(res = ft_strnew(len)))
 		return (NULL);
-	if (len1 >= len2)
-		ft_memcpy(res, s1, len - 1);
-	else
+	while (len != 0 && s1 && s2)
 	{
-		ft_memcpy(res, s2, len - 1);
-		flag = 1;
-	}
-	while (s1[i] != '\0' || s2[i] != '\0')
-	{
-		if ((s1[i] + s2[i] + hold - 48) <= 57)
+		if ((s1[len] + s2[len] + hold - 48) <= 57)
 		{
-			res[i] = s1[i] + s2[i] - 48 + hold;
+			res[len] = s1[len] + s2[len] - 48 + hold;
 			hold = 0;
 		}
 		else
 		{
-			res[i] = '0';
+			res[len] = '0';
 			hold = 1;
 		}
-		printf("res[%d] = %c\n", i, res[i]);
-		i++;
+		len--;
+		printf("res[%d] = %c\n", len, res[len]);
 	}
-	return (res);
+	if (s1[len] == '\0' && s2)
+	{
+		j = len;
+		while (s2[j] != '\0')
+			res[len] = s2[j++];
+		printf("j = %d\n", j);
+	}
+	else if (s2[len] == '\0' && s1)
+	{
+		j = len;
+		while (s1)
+			res[len--] = s1[j--];
+	}
+/*	
+
+
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+	if ((s1[i] + s2[i] + hold - 48) <= 57)
+	{
+	res[i] = s1[i] + s2[i] - 48 + hold;
+	hold = 0;
+	}
+	else
+	{
+	res[i] = '0';
+	hold = 1;
+	}
+	i++;
+	if (s1[i] == '\0' && s2[i] != '\0')
+	{
+	j = i;
+	while (s2[j] != '\0')
+	res[i++] = s2[j++];
+	printf("j = %d\n", j);
+	}
+	else if (s2[i] == '\0' && s1[i] != '\0')
+	{
+	j = i;
+	while (s1[j++] != '\0')
+	res[j] = s1[j];
+	}
+	printf("res[%d] = %c\n", i, res[i]);
+	}
+	*/
+return (res);
 }
--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 17:57:47 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/04/04 17:36:01 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/04/04 21:32:11 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ int	main(void)
 	printf("LDBL_MAX = %LF\n", LDBL_MAX);*/
 	double f;
 	int		exp;
-	char *s1 = "1";
-	char *s2 = "123456";
+	char *s1 = "1\0";
+	char *s2 = "123456\0";
 
 	f = -25.3;
 	printf("frexp = %f\n, *exp = %d\n", frexp(f, &exp), exp);
